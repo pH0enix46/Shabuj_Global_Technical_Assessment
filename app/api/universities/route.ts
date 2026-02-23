51ec51c43d1cdc54c529ab07ea3d64a7e9dddc1e
pH0enix46 <mdjoy.javascript@gmail.com>
2026-02-23 11:37:08 +0600

    Solving vercel deploy and chnage cache behaviour

--- app/api/universities/route.ts
+++ app/api/universities/route.ts
@@ -3,13 +3,11 @@ import { getUniversities } from "@/_server/data/data";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+  const params = Object.fromEntries(searchParams.entries());
+
   try {
-    const { searchParams } = new URL(request.url);
-
-    // Convert searchParams to a plain object
-    const params = Object.fromEntries(searchParams.entries());
-
-    // Fetch data using the shared logic in actions.ts
+    // Fetch data using the shared logic in data.ts
     const result = await getUniversities(params);
 
     if (result.error) {
